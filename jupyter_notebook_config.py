e566285efa498c455e908781273c797dde98e47b
Kirill Shmakov <shmakir@yandex.ru>
2025-10-04 17:28:54 +0200

    Updated jupyter config

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -6,4 +6,7 @@ c.NotebookApp.token = ''      # Disable token auth
 c.NotebookApp.password = ''   # Disable password auth
 c.NotebookApp.open_browser = False
 
-c.NotebookApp.notebook_dir = '/home/kishmakov/Repos/shad/ml-2025-fall/hw01'
+c.NotebookApp.notebook_dir = '/home/kishmakov/Repos/shad/ml-2025-fall'
+
+c.NotebookApp.ip = '0.0.0.0'    # <-- allow access from local network
+c.NotebookApp.port = 8888       # (optional) specify port if you want
